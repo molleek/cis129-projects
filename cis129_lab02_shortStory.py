005d08e21c93725196d68a8489df4225f74f4237
molleek <mkahan@mail.pima.edu>
2024-06-06 05:29:19 -0700

    Update cis129_lab02_shortStory.py
    This update removes print Hello World and adds code that prints a short text based story about Rapunzel.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author:
+# First Python lab in CIS129 course
+# Delets Hellow World and prints a simple short story
+print('Once upon a time there lived a princess named Rapunzel.')
+print('Rapunzel was locked away in a tall tower by her mother')
+Print('Everything changes when a strong prince name Flynn Ryder rescued her.')
+print('Once she was saved, the couple got married and lived happily ever after.') 
