16318c462059dfd0af4394156f756af40f769f42
molleek <mkahan@mail.pima.edu>
2024-06-06 05:30:18 -0700

    Update cis129_lab02_shortStory.py
    the p in print was uncapitalized in line 6

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -3,5 +3,5 @@
 # Delets Hellow World and prints a simple short story
 print('Once upon a time there lived a princess named Rapunzel.')
 print('Rapunzel was locked away in a tall tower by her mother')
-Print('Everything changes when a strong prince name Flynn Ryder rescued her.')
+print('Everything changes when a strong prince name Flynn Ryder rescued her.')
 print('Once she was saved, the couple got married and lived happily ever after.') 
