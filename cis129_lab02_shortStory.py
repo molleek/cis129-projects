3d4307dfa1c072fbc367256f2ec590f29e01c0e3
molleek <mkahan@mail.pima.edu>
2024-06-06 05:34:05 -0700

    Update cis129_lab02_shortStory.py
    updates comment

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,7 +1,7 @@
 # Author: Mollee Kahan
 # First Python lab in CIS129 course
-# Delets Hellow World and prints a simple short story
+# Prints a simple short story about Rapunzel
 print('Once upon a time there lived a princess named Rapunzel.')
 print('Rapunzel was locked away in a tall tower by her mother')
 print('Everything changes when a strong prince name Flynn Ryder rescued her.')
-print('Once she was saved, the couple got married and lived happily ever after.') 
+print('Once she was saved, the couple got married and lived happily ever after.')
